cbcf52e098cd8732187dee9124c24e514c631cb6
Atai <ataibotaliev@gmail.com>
2021-06-23 12:57:36 +0600

    add slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,10 @@
-import ProgramSlider from './slick.js';
-ProgramSlider();
-import './click.js';
+$('.slider__item ').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,  
+  autoplaySpeed: 2000,
+});
---
+++ src/js/slick.js
@@ -0,0 +1,6 @@
+$('.slider__item').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    // autoplay: true,
+    // autoplaySpeed: 4000,
+});
